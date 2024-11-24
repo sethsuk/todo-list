808fdefdc48f8632adc9621bf53418455a5c811f
Seth Sukboontip <sukboontips@gmail.com>
2024-11-24 14:48:38 -0500

    added environmental variables for deployment

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'tododb', 
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME, 
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
